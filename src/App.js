d16d192f7d74191c8449e87bc95c69e006b7b642
Eureka <eureka@MacBook-Pro-2.local>
2022-02-18 23:32:00 +0400

    logo changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './cyber-security.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Devendra K Meena, an aspiring Cybersecurity Professional
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Website In Progress
         </a>
       </header>
     </div>
